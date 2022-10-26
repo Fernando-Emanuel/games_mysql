1e7a09bb0f62180cedf70236fffcd4f93850394a
Fernando-Emanuel <fernando2004emanuel@gmail.com>
2022-10-26 20:49:19 +0000

    Generos

---
+++ app/src/main/java/fernandoemanuel/repositories/GeneroRepository.java
@@ -0,0 +1,8 @@
+package fernandoemanuel.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import fernandoemanuel.models.Genero;
+
+public interface GeneroRepository extends CrudRepository<Genero, Integer> {
+}
